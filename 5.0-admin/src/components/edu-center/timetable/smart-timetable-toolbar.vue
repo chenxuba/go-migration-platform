0289ef92d54e76599f478f930e22bb2dd0232ccf
陈瑞生 <1115009958@qq.com>
2026-04-15 18:24:15 +0800

    优化课表上面的操作栏

--- 5.0-admin/src/components/edu-center/timetable/smart-timetable-toolbar.vue
+++ 5.0-admin/src/components/edu-center/timetable/smart-timetable-toolbar.vue
@@ -195,184 +195,189 @@ const isCurrentRange = computed(() => {
 
 <template>
   <div class="time-template mt2 bg-white py3 px5 rounded-4 rounded-lb-0 rounded-rb-0">
-    <div class="top-filter st-top-filter-bar flex flex-nowrap items-center gap-1 overflow-x-auto">
-      <div class="shrink-0">
-        <a-radio-group v-model:value="currentModelValue" button-style="solid">
-          <a-radio-button value="1">
-            1v1
-          </a-radio-button>
-          <a-radio-button value="2">
-            班课
-          </a-radio-button>
-        </a-radio-group>
-      </div>
+    <div class="top-filter st-top-filter-bar">
+      <div class="st-top-filter-bar__inner">
+        <div class="st-top-filter-bar__left">
+          <div class="shrink-0">
+            <a-radio-group v-model:value="currentModelValue" button-style="solid">
+              <a-radio-button value="1">
+                1v1
+              </a-radio-button>
+              <a-radio-button value="2">
+                班课
+              </a-radio-button>
+            </a-radio-group>
+          </div>
 
-      <div class="shrink-0">
-        <div v-show="currentModel === '1'" class="flex items-center shrink-0 gap-1">
-          <span class="whitespace-nowrap w-71px text-right">选择1v1：</span>
-          <a-select
-            v-model:value="oneToOneValue"
-            v-model:open="oneToOneOpenValue"
-            allow-clear
-            show-search
-            :loading="oneToOneListLoading"
-            :dropdown-match-select-width="false"
-            :dropdown-style="{ width: '820px' }"
-            :dropdown-render="renderOneToOneDropdown"
-            :filter-option="filterOneToOneOption"
-            placeholder="搜索/选择"
-            class="st-top-1v1-select"
-            popup-class-name="st-top-1v1-select-dropdown"
-            option-label-prop="label"
-            @dropdown-visible-change="onOneToOneDropdownVisibleChange"
-            @change="onOneToOneChange"
-          >
-            <a-select-option
-              v-for="item in oneToOneData"
-              :key="item.id"
-              :value="item.id"
-              :label="item.name"
-            >
-              <div>{{ item.name }}</div>
-            </a-select-option>
-          </a-select>
-        </div>
-
-        <div v-show="currentModel === '2'" class="flex items-center">
-          <span class="w-75px">选择班级：</span>
-          <a-select
-            v-model:value="classIdValue"
-            v-model:open="classPickerOpenValue"
-            allow-clear
-            show-search
-            :filter-option="false"
-            :loading="classListLoading"
-            :dropdown-match-select-width="false"
-            :dropdown-style="{ width: '820px' }"
-            :dropdown-render="renderClassDropdown"
-            placeholder="请搜索/选择班级"
-            class="st-top-class-select"
-            option-label-prop="label"
-            popup-class-name="st-top-1v1-select-dropdown"
-            @dropdown-visible-change="onClassDropdownVisibleChange"
-            @search="onClassSearch"
-            @change="onClassChange"
-          >
-            <a-select-option
-              v-for="item in classData"
-              :key="item.id"
-              :value="item.id"
-              :data="item"
-              :label="item.name"
-            >
-              <div>{{ item.name }}</div>
-              <div class="text-3 text-#666">
-                主教：{{ item.mainTeacherName }}
-              </div>
-            </a-select-option>
-          </a-select>
-        </div>
-      </div>
-
-      <div class="time-selector flex items-center shrink-0 st-time-selector--after-filters">
-        <a-select
-          v-model:value="currentTimeValue"
-          :options="timeViewOptions"
-          class="st-time-view-select"
-        />
-        <div
-          class="font-800 text-5 flex items-center shrink-0 st-date-nav"
-          :class="
-            currentTime === 'day'
-              ? 'st-date-nav--day'
-              : isWeekLikeView
-                ? 'st-date-nav--week'
-                : 'st-date-nav--month'
-          "
-        >
-          <a-popover trigger="hover">
-            <template #content>
-              {{ currentTime === 'day' ? '前一天' : isWeekLikeView ? '上一周' : '上个月' }}
-            </template>
-            <span
-              class="cursor-pointer text-3 text-#888 flex w6 h6 bg-#eee rounded-10 flex-center font-500 shrink-0 hover-text-#06f hover-bg-#e6f0ff"
-              @click="onPrev"
-            >
-              <LeftOutlined />
-            </span>
-          </a-popover>
-          <span class="mx-1 min-w-0 flex-1 st-date-nav__mid">
-            <div
-              class="relative cursor-pointer whitespace-nowrap text-center st-date-nav__text"
-              :class="isCurrentRange ? 'text-#0061ff' : 'text-#222'"
-            >
-              {{ formatDateRange(currentWeek) }}
-              <a-date-picker
-                v-if="currentTime === 'day'"
-                v-model:value="currentWeekValue"
-                class="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0"
-                :allow-clear="false"
-                :bordered="false"
-                :format="formatDateRange"
-                style="cursor:pointer;"
-              />
-              <a-date-picker
-                v-else-if="isWeekLikeView"
-                v-model:value="currentWeekValue"
-                class="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0"
-                picker="week"
-                :allow-clear="false"
-                :bordered="false"
-                :format="formatDateRange"
-                style="cursor:pointer;"
-              />
-              <a-date-picker
-                v-else
-                v-model:value="currentWeekValue"
-                class="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0"
-                picker="month"
-                :allow-clear="false"
-                :bordered="false"
-                :format="formatDateRange"
-                style="cursor:pointer;"
-              />
+          <div class="shrink-0">
+            <div v-show="currentModel === '1'" class="flex items-center shrink-0 gap-1">
+              <span class="whitespace-nowrap w-71px text-right">选择1v1：</span>
+              <a-select
+                v-model:value="oneToOneValue"
+                v-model:open="oneToOneOpenValue"
+                allow-clear
+                show-search
+                :loading="oneToOneListLoading"
+                :dropdown-match-select-width="false"
+                :dropdown-style="{ width: '820px' }"
+                :dropdown-render="renderOneToOneDropdown"
+                :filter-option="filterOneToOneOption"
+                placeholder="搜索/选择"
+                class="st-top-1v1-select"
+                popup-class-name="st-top-1v1-select-dropdown"
+                option-label-prop="label"
+                @dropdown-visible-change="onOneToOneDropdownVisibleChange"
+                @change="onOneToOneChange"
+              >
+                <a-select-option
+                  v-for="item in oneToOneData"
+                  :key="item.id"
+                  :value="item.id"
+                  :label="item.name"
+                >
+                  <div>{{ item.name }}</div>
+                </a-select-option>
+              </a-select>
             </div>
-          </span>
-          <a-popover trigger="hover">
-            <template #content>
-              {{ currentTime === 'day' ? '后一天' : isWeekLikeView ? '下一周' : '下个月' }}
-            </template>
-            <span
-              class="cursor-pointer text-3 text-#888 flex w6 h6 bg-#eee rounded-10 flex-center font-500 shrink-0 hover-text-#06f hover-bg-#e6f0ff"
-              @click="onNext"
-            >
-              <RightOutlined />
-            </span>
-          </a-popover>
-        </div>
-        <a-button size="small" class="shrink-0 st-this-week-btn" @click="onThisWeek">
-          本周
-        </a-button>
-      </div>
 
-      <div class="ml-auto flex shrink-0 items-center gap-2">
-        <a-select
-          v-if="useCompactGroupSwitcher"
-          v-model:value="currentGroupValue"
-          :options="groupOptions.map(opt => ({ value: opt.key, label: opt.label }))"
-          class="st-group-select"
-        />
-        <a-radio-group v-else v-model:value="currentGroupValue" button-style="solid">
-          <a-radio-button v-for="opt in groupOptions" :key="opt.key" :value="opt.key">
-            {{ opt.label }}
-          </a-radio-button>
-        </a-radio-group>
-        <a-space>
-          <CreateSchedulePopover />
-          <a-button :loading="exportLoading" @click="onExport">
-            导出课表
+            <div v-show="currentModel === '2'" class="flex items-center">
+              <span class="w-75px">选择班级：</span>
+              <a-select
+                v-model:value="classIdValue"
+                v-model:open="classPickerOpenValue"
+                allow-clear
+                show-search
+                :filter-option="false"
+                :loading="classListLoading"
+                :dropdown-match-select-width="false"
+                :dropdown-style="{ width: '820px' }"
+                :dropdown-render="renderClassDropdown"
+                placeholder="请搜索/选择班级"
+                class="st-top-class-select"
+                option-label-prop="label"
+                popup-class-name="st-top-1v1-select-dropdown"
+                @dropdown-visible-change="onClassDropdownVisibleChange"
+                @search="onClassSearch"
+                @change="onClassChange"
+              >
+                <a-select-option
+                  v-for="item in classData"
+                  :key="item.id"
+                  :value="item.id"
+                  :data="item"
+                  :label="item.name"
+                >
+                  <div>{{ item.name }}</div>
+                  <div class="text-3 text-#666">
+                    主教：{{ item.mainTeacherName }}
+                  </div>
+                </a-select-option>
+              </a-select>
+            </div>
+          </div>
+
+          <a-select
+            v-model:value="currentTimeValue"
+            :options="timeViewOptions"
+            class="st-time-view-select"
+          />
+        </div>
+
+        <div class="time-selector st-top-filter-bar__center">
+          <div
+            class="font-800 text-5 flex items-center shrink-0 st-date-nav"
+            :class="
+              currentTime === 'day'
+                ? 'st-date-nav--day'
+                : isWeekLikeView
+                  ? 'st-date-nav--week'
+                  : 'st-date-nav--month'
+            "
+          >
+            <a-popover trigger="hover">
+              <template #content>
+                {{ currentTime === 'day' ? '前一天' : isWeekLikeView ? '上一周' : '上个月' }}
+              </template>
+              <span
+                class="cursor-pointer text-3 text-#888 flex w6 h6 bg-#eee rounded-10 flex-center font-500 shrink-0 hover-text-#06f hover-bg-#e6f0ff"
+                @click="onPrev"
+              >
+                <LeftOutlined />
+              </span>
+            </a-popover>
+            <span class="mx-1 min-w-0 flex-1 st-date-nav__mid">
+              <div
+                class="relative cursor-pointer whitespace-nowrap text-center st-date-nav__text"
+                :class="isCurrentRange ? 'text-#0061ff' : 'text-#222'"
+              >
+                {{ formatDateRange(currentWeek) }}
+                <a-date-picker
+                  v-if="currentTime === 'day'"
+                  v-model:value="currentWeekValue"
+                  class="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0"
+                  :allow-clear="false"
+                  :bordered="false"
+                  :format="formatDateRange"
+                  style="cursor:pointer;"
+                />
+                <a-date-picker
+                  v-else-if="isWeekLikeView"
+                  v-model:value="currentWeekValue"
+                  class="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0"
+                  picker="week"
+                  :allow-clear="false"
+                  :bordered="false"
+                  :format="formatDateRange"
+                  style="cursor:pointer;"
+                />
+                <a-date-picker
+                  v-else
+                  v-model:value="currentWeekValue"
+                  class="absolute top-0 left-0 right-0 bottom-0 z-10 opacity-0"
+                  picker="month"
+                  :allow-clear="false"
+                  :bordered="false"
+                  :format="formatDateRange"
+                  style="cursor:pointer;"
+                />
+              </div>
+            </span>
+            <a-popover trigger="hover">
+              <template #content>
+                {{ currentTime === 'day' ? '后一天' : isWeekLikeView ? '下一周' : '下个月' }}
+              </template>
+              <span
+                class="cursor-pointer text-3 text-#888 flex w6 h6 bg-#eee rounded-10 flex-center font-500 shrink-0 hover-text-#06f hover-bg-#e6f0ff"
+                @click="onNext"
+              >
+                <RightOutlined />
+              </span>
+            </a-popover>
+          </div>
+          <a-button size="small" class="shrink-0 st-this-week-btn" @click="onThisWeek">
+            本周
           </a-button>
-        </a-space>
+        </div>
+
+        <div class="st-top-filter-bar__right">
+          <a-select
+            v-if="useCompactGroupSwitcher"
+            v-model:value="currentGroupValue"
+            :options="groupOptions.map(opt => ({ value: opt.key, label: opt.label }))"
+            class="st-group-select"
+          />
+          <a-radio-group v-else v-model:value="currentGroupValue" button-style="solid">
+            <a-radio-button v-for="opt in groupOptions" :key="opt.key" :value="opt.key">
+              {{ opt.label }}
+            </a-radio-button>
+          </a-radio-group>
+          <a-space>
+            <CreateSchedulePopover />
+            <a-button :loading="exportLoading" @click="onExport">
+              导出课表
+            </a-button>
+          </a-space>
+        </div>
       </div>
     </div>
   </div>
@@ -387,6 +392,49 @@ const isCurrentRange = computed(() => {
 .st-top-class-select {
   width: 180px;
   max-width: 180px;
+}
+
+.st-top-filter-bar {
+  overflow-x: auto;
+  overflow-y: hidden;
+  scrollbar-width: thin;
+  -webkit-overflow-scrolling: touch;
+}
+
+.st-top-filter-bar__inner {
+  display: grid;
+  grid-template-columns: max-content minmax(8px, 1fr) auto minmax(8px, 1fr) max-content;
+  align-items: center;
+  width: max-content;
+  min-width: 100%;
+  column-gap: 6px;
+  row-gap: 8px;
+}
+
+.st-top-filter-bar__left,
+.st-top-filter-bar__right {
+  display: flex;
+  align-items: center;
+  min-width: max-content;
+  gap: 4px;
+}
+
+.st-top-filter-bar__left {
+  grid-column: 1;
+  justify-content: flex-start;
+}
+
+.st-top-filter-bar__center {
+  grid-column: 3;
+  display: flex;
+  align-items: center;
+  justify-self: center;
+  min-width: max-content;
+}
+
+.st-top-filter-bar__right {
+  grid-column: 5;
+  justify-content: flex-end;
 }
 
 .st-time-view-select {
@@ -425,11 +473,6 @@ const isCurrentRange = computed(() => {
   display: block;
   overflow: hidden;
   text-overflow: ellipsis;
-}
-
-.st-top-filter-bar {
-  scrollbar-width: thin;
-  -webkit-overflow-scrolling: touch;
 }
 
 .time-selector {
